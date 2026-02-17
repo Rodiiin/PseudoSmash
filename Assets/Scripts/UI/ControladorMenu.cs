a1f791146ff44e590148ef54aa6d95c7796240c2
Rodin <aragonleorodin@hotmail.com>
2026-02-17 17:00:34 -0600

    Conexión_del_menú_al_selectorDePersonaje

--- Assets/Scripts/UI/ControladorMenu.cs
+++ Assets/Scripts/UI/ControladorMenu.cs
@@ -10,7 +10,13 @@ public class ControladorMenu : MonoBehaviour
     public CanvasGroup grupoAlfa; // Arrastra aquí el SubMenu_Modos
     private bool enSubMenu = false; // Para saber si estamos viendo la flecha "<-" o el texto "Jugar"
     
+    [Header("Nuevas Secciones")]
+    public GameObject panelMenuPrincipal; // El objeto que contiene el Fondo, Cometas, etc.
+    public GameObject panelSeleccionPersonaje;     // El objeto Panel_SeleccionPersonaje que acabas de crear
 
+    //Herrameintas de desvanecimiento
+    public CanvasGroup grupoMenuPrincipalCG;
+    public CanvasGroup grupoSeleccionPersonajeCG;
     public void AlDarClickEnJugar()
     {
         StopAllCoroutines(); // Detiene transiciones a medias
@@ -47,5 +53,47 @@ public class ControladorMenu : MonoBehaviour
         grupoAlfa.alpha = objetivoAlfa;
         if (!activar) subMenuModos.SetActive(false);
     }
+
+
+    public void IrASeleccionPersonaje()
+    {
+        StartCoroutine(TransicionEntrePaneles(grupoMenuPrincipalCG, grupoSeleccionPersonajeCG, true));
+    }
+
+    public void RegresarAlMenuPrincipalDesdeSeleccionPersonaje()
+    {
+        StartCoroutine(TransicionEntrePaneles(grupoSeleccionPersonajeCG, grupoMenuPrincipalCG, false));
+    }
+
+    // Esta función hace el cambio suave
+IEnumerator TransicionEntrePaneles(CanvasGroup saliente, CanvasGroup entrante, bool esHaciaSeleccion)
+{
+    float t = 0;
+    float duracionTransicion = 0.5f;
+
+    // 1. El panel que está visible empieza a desvanecerse
+    while (t < duracionTransicion)
+    {
+        t += Time.deltaTime;
+        saliente.alpha = 1 - (t / duracionTransicion);
+        yield return null;
+    }
+    saliente.alpha = 0;
+    saliente.gameObject.SetActive(false);
+
+    // 2. Activamos el nuevo panel y lo desvanecemos hacia adentro
+    entrante.gameObject.SetActive(true);
+    t = 0;
+    while (t < duracionTransicion)
+    {
+        t += Time.deltaTime;
+        entrante.alpha = t / duracionTransicion;
+        yield return null;
+    }
+    entrante.alpha = 1;
+}
+
+
+
 
 }
